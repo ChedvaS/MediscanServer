c1028d0f1a223532b25c7071085f4ffbde33e53e
ChedvaS <ch0548460015@gmail.com>
2022-01-19 21:38:54 +0200

    שליפת רשימת התרופות

--- MediscanBackend/Controllers/MedicineStockController.cs
+++ MediscanBackend/Controllers/MedicineStockController.cs
@@ -4,14 +4,20 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Bl;
 
+//aaa
 namespace MediscanBackend.Controllers
 {
+    [RoutePrefix("api/medicineStock")]
     public class MedicineStockController : ApiController
     {
-
-
-
-
+        // שליפת רשימת מלאי התרופות לפי קוד
+        [HttpGet]
+        [Route("GetmedicineStockList/{idMedicine}")]
+        public IHttpActionResult GetmedicineStockList(int idMedicine)
+        {
+            return Ok(medicinestockBl.Getmedicinestock(idMedicine));
+        }
     }
 }
